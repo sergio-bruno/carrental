200dfd9f3fef1f8db8c20c8393e9267b8035f47a
sergio bruno <sergio.bruno.ti@gmail.com>
2021-01-08 17:39:45 -0300

    Implementing Swagger

---
+++ src/main/java/com/slbruno/carrental/controller/CarRentalController.java
@@ -0,0 +1,15 @@
+package com.slbruno.carrental.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cr/cars")
+public class CarRentalController {
+
+	@GetMapping
+	public String Hello() {
+		return "Hi, car rental ok";
+	}
+}
