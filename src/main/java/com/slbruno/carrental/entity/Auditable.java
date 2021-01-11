e1f33c0defb4354b5e52b9df443a12e57ed0b36d
sergio bruno <sergio.bruno.ti@gmail.com>
2021-01-11 16:56:50 -0300

    Added packages

---
+++ src/main/java/com/slbruno/carrental/entity/Auditable.java
@@ -0,0 +1,35 @@
+package com.slbruno.carrental.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+
+    @CreatedBy
+    protected String createdBy;
+
+    @LastModifiedBy
+    protected String lastModifiedBy;
+
+    @CreatedDate
+    @Column(nullable = false)
+    protected LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column
+    protected LocalDateTime lastModifiedDate;
+}
